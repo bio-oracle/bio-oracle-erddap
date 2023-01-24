ddb7242712e1f05d869d822b36863fb7861bb833
Vini Salazar <vinicius.salazar@unimelb.edu.au>
2023-01-24 07:40:01 +0000

    Mount correct data directories
      - Use actual data directories on container
      - Add $@ so it's possible to pass args to GenerateDatasetsXml
      - Print command

--- GenerateDatasetsXml.sh
+++ GenerateDatasetsXml.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+CMD="bash GenerateDatasetsXml.sh -verbose $@"
+echo $CMD
 docker run --rm -it \
-  -v "$(pwd)/datasets:/datasets" \
+  -v "/data/layers:/datasets" \
   -v "$(pwd)/logs:/erddapData/logs" \
   axiom/docker-erddap:2.18 \
-  bash -c "cd webapps/erddap/WEB-INF/ && bash GenerateDatasetsXml.sh -verbose"
+  bash -c "cd webapps/erddap/WEB-INF/ && $CMD"
