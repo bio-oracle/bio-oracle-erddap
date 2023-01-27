4dfc0814c380d44bc29b994db86557f864907749
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2023-01-27 14:41:10 +1100

    Add backup of datasets files

--- utils/CollateGenerateDatasetsXml.py
+++ utils/CollateGenerateDatasetsXml.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
+from datetime import datetime
 from glob import glob
+from pathlib import Path
+from shutil import copyfile
 import xml.etree.ElementTree as ET
 
 from tqdm import tqdm
@@ -18,10 +21,17 @@ def main():
             snip_tree = ET.parse(xml)
             snip_root = snip_tree.getroot()
             main_root.append(snip_root)
-        except ET.ParseError:
+        except ET.ParseError:  # for empty files
             pass
 
     datasets_file = "./erddap/content/datasets.xml"
+
+    backup = lambda file: copyfile(file, Path("./erddap/content/history/").joinpath(datetime.now().strftime("%Y-%m-%d-%T") + "." + str(Path(file.name))))
+    
+    # Back up the old file
+    backup(datasets_file)
+
+    # Write the new file
     main_tree.write(datasets_file, encoding="utf-8")
 
     xml_string = '<?xml version="1.0" encoding="ISO-8859-1" ?>'
@@ -32,6 +42,8 @@ def main():
         f.write(xml_string + "\n")
         f.write(r)
 
+    # Back up the new file
+    backup(datasets_file)
 
 if __name__ == "__main__":
     main()
