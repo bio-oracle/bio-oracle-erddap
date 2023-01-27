f3e2973bf943767f24f19f2b4fbafa41315793ad
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2023-01-27 15:13:03 +1100

    Touch new datasets so they can be refreshed.

--- utils/LoopGenerateDatasetsXml.py
+++ utils/LoopGenerateDatasetsXml.py
@@ -10,6 +10,9 @@ from CollateGenerateDatasetsXml import main as collate
 
 def main():
     layers = glob("/data/layers/*/")
+
+    print(f"Found {len(layers)} layers.")
+    print(f"Processing layers. Files will be created in the 'logs/datasets' directory.")
     for layer in tqdm(layers):
         layer_basename = layer.split("/")[-2].rstrip("/")
         p = subprocess.Popen(process_layer(layer).split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
