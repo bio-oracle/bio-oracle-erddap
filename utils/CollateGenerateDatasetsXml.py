e3adcdf26c6342a6a87f7a7d60a93a28704cb92e
Vini Salazar <vinicius.salazar@unimelb.edu.au>
2023-03-13 04:10:06 +0000

    Update XML string
     - Use UTF-8 encoding

--- utils/CollateGenerateDatasetsXml.py
+++ utils/CollateGenerateDatasetsXml.py
@@ -104,7 +104,7 @@ def main():
             f.write(line)
 
 
-    xml_string = '<?xml version="1.0" encoding="ISO-8859-1" ?>'
+    xml_string = '<?xml version="1.0" encoding="UTF-8" ?>'
 
     with open(datasets_file, "r+") as f:
         r = f.read()
