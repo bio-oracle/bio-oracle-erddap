609d6520e14ea1e2e97663ae0223fba3d783175d
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2023-08-31 15:06:02 +1000

    Update script
      - Add options between filename/dir to get layer name

--- utils/LoopGenerateDatasetsXml.py
+++ utils/LoopGenerateDatasetsXml.py
@@ -8,6 +8,7 @@ script.
 import argparse
 from glob import glob
 from shutil import copyfile
+from pathlib import Path
 import subprocess
 
 from CollateGenerateDatasetsXml import main as collate
@@ -31,19 +32,20 @@ def main(args):
     for ix, layer in enumerate(layers):
         # if ((ix > 0) and (ix % 10 == 0)) or (ix + 1 == n_layers):
         print(f"Processed {ix}/{n_layers} layers.", end="\r")
-        layer_basename = layer.split("/")[-2].rstrip("/")
-        layer_dir = "/".join(layer.split("/")[:-1])
-        filename = layer.split("/")[-1][:-3]
-        cmd = process_layer(layer_dir, filename).split()
+        path_layer = Path(layer)
+        layer_parent_dir = path_layer.parent
+        layer_grandparent_dir = layer_parent_dir.parent
+        filename = path_layer.stem if args.layer_name_from == "filename" else layer_parent_dir.name
+        cmd = process_layer(layer_grandparent_dir, filename).split()
         if not args.dry_run:
             p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             stdout, stderr = p.communicate()
-            with open(f"logs/datasets/logs/{layer_basename}_{filename}_stdout.log", "w") as f:
+            with open(f"logs/datasets/logs/{layer_parent_dir}_{filename}_stdout.log", "w") as f:
                 f.write("stdout:\n")
                 f.write(stdout.decode())
                 f.write("stderr:\n")
                 f.write(stderr.decode())
-            outfile_xml = "logs/datasets/" + layer_basename + "_" + filename + ".xml"
+            outfile_xml = "logs/datasets/" + filename + ".xml" if args.layer_name_from == "filename" else "logs/datasets/" + layer_parent_dir + "_" + filename + ".xml"
             copyfile("logs/GenerateDatasetsXml.out", outfile_xml)
         else:
             print(" ".join(cmd))
@@ -64,6 +66,7 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--include", help="INCLUDE strings, comma-separated. Only include datasets that contain these strings in their ID.", default=None, required=False)
     parser.add_argument("-e", "--exclude", help="EXCLUDE strings, comma-separated. Exclude datasets that contain these strings in their ID.", default=None, required=False)
     parser.add_argument("-g", "--glob-string", help="Glob string to be used to look for files.", default="/data/layers/*/*.nc", required=False)
+    parser.add_argument("--layer-name-from", help="Whether to use the layer name from the filename or from the parent directory.", choices=["filename", "parent"], default="parent", required=False)
     parser.add_argument("--skip-collate", help="Only generate the XML snippets, don't collate them with the CollateGenerateDatasetsXml.py script.", action="store_true", required=False)
     parser.add_argument("--dry-run", help="Only print the GenerateDatasetsXml.sh command and don't run anything.", action="store_true", required=False)
     args = parser.parse_args()
