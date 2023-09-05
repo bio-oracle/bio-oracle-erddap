aa8424c13be4cadc2efd5a6c3f7af7182af8c613
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2023-09-05 14:53:39 +1000

    Update GenerateDatasetsXml.sh
      - Use '' to get all args
      - Use variables from .env

--- GenerateDatasetsXml.sh
+++ GenerateDatasetsXml.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+source .env
 CMD="bash GenerateDatasetsXml.sh -verbose $@"
 echo $CMD
 docker run --rm -it \
-  -v "/data/layers:/datasets" \
-  -v "$(pwd)/logs:/erddapData/logs" \
-   -v $(pwd)/erddap/content:/usr/local/tomcat/content/erddap \
+  -v "${DATA_DIR}:/datasets" \
+  -v "${PWD}/logs:/erddapData/logs" \
+  -v ${PWD}/erddap/content:/usr/local/tomcat/content/erddap \
   axiom/docker-erddap:2.23-jdk17-openjdk \
-  bash -c "cd webapps/erddap/WEB-INF/ && bash GenerateDatasetsXml.sh -verbose"
+  bash -c "cd webapps/erddap/WEB-INF/ && eval $CMD"
